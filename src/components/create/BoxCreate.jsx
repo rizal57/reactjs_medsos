de99661ddec4c3101a583584305004a7fc0441b1
rizal57 <fahrurrzl5798@gmail.com>
2023-03-31 17:07:31 +0700

    [ADD] Add preview image

--- src/components/create/BoxCreate.jsx
+++ src/components/create/BoxCreate.jsx
@@ -2,6 +2,7 @@ import { BiSmile } from "react-icons/bi";
 import { AiOutlineFileGif } from "react-icons/ai"
 import { RiGalleryFill } from "react-icons/ri";
 import { GoLocation } from "react-icons/go";
+import { MdClose } from "react-icons/md";
 import { ButtonPrimary } from "../button";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
@@ -13,11 +14,22 @@ import { useState } from "react";
 const BoxCreate = () => {
   const [post, setPost] = useState('')
   const [showEmoji, setShowEmoji] = useState(false)
+  const [previewImage, setPreviewImage] = useState(null)
 
   const { authUser } = useContext(AuthContext)
 
   const onChange = (e) => {
     setPost(e.target.value)
+  }
+
+  const handlePreviewImage = (e) => {
+    const file = e.target.files[0]
+
+    const reader = new FileReader()
+    reader.onload = (res) => {
+      setPreviewImage(res.target.result)
+    }
+    reader.readAsDataURL(file)
   }
 
   const showEmojiHendler = () => {
@@ -36,16 +48,47 @@ const BoxCreate = () => {
     <div className="w-full px-4 mb-4">
       <div className="p-4 rounded-lg bg-white shadow-md">
         <div className="flex gap-3">
-          <img src={authUser?.photoURL ? authUser?.photoURL : NoPoto} alt="User Image" className="w-10 h-10 rounded-full" />
+          <img
+            src={authUser?.photoURL ? authUser?.photoURL : NoPoto}
+            alt="User Image"
+            className="w-10 h-10 rounded-full"
+          />
 
           <div className="w-full">
-            <textarea name="post" id="post" value={post} onChange={onChange} className="resize-none outline-none border-none w-full placeholder:text-xl" placeholder="What's happening?"></textarea>
+            <textarea
+              name="post"
+              id="post"
+              value={post}
+              onChange={onChange}
+              className="resize-none outline-none border-none w-full placeholder:text-xl"
+              placeholder="What's happening?"
+            ></textarea>
+            {previewImage && (
+              <div className="my-4 relative w-full md:w-[250px]">
+                <div onClick={() => setPreviewImage(null)} className="cursor-pointer transition duration-300 bg-slate-800/60 hover:bg-slate-800 text-white w-6 h-6 flex items-center justify-center rounded-full absolute top-1 right-1">
+                  <MdClose />
+                </div>
+                <img src={previewImage} alt="Image Preview" className="w-full object-cover rounded-lg" />
+              </div>
+            )}
 
             <div className="flex justify-between items-center text-violet-500 text-lg md:text-xl mt-3">
               <div className="flex gap-4 md:gap-6">
-                <div className="cursor-pointer relative">
-                  <RiGalleryFill />
+                {/* image */}
+                <div className="relative">
+                  <input
+                    type="file"
+                    id='img'
+                    className="hidden"
+                    accept="image/jpg, image/jpeg, image/png, image/gif"
+                    onChange={handlePreviewImage}
+                  />
+
+                  <label htmlFor="img" className="cursor-pointer">
+                    <RiGalleryFill />
+                  </label>
                 </div>
+
                 <div className="cursor-pointer relative">
                   <AiOutlineFileGif />
                 </div>
@@ -65,7 +108,7 @@ const BoxCreate = () => {
               </div>
 
               <div>
-                <ButtonPrimary text='Post' />
+                <ButtonPrimary disabled={!post} opacity={`${!post && 'opacity-50'}`} text='Post' />
               </div>
 
             </div>
